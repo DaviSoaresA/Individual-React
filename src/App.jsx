92ca4efc0178750de80989b67784280bd87a294f
DaviSoaresA <didequinho@gmail.com>
2024-10-30 16:46:13 -0300

    Mudanças no App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Calculadora from "./components/Calculadora";
+import "./App.css"
 
 export default function App() {
   const [peso, setPeso] = useState(0);
@@ -11,13 +12,13 @@ export default function App() {
     const imc = peso/(altura*altura);
     if (imc < 18.5) {
       return "Baixo peso";
-    } else if (imc >= 18.5 && imc <= 24.9) {
+    } else if (imc > 18.4 && imc <= 24.9) {
       return "Eutrofia(Peso Ideal)";
-    } else if (imc >= 25.0 && imc <= 29.9) {
+    } else if (imc > 24.9 && imc <= 29.9) {
       return "Sobrepeso";
-    } else if (imc >= 30.0 && imc <= 34.9) {
+    } else if (imc > 29.9 && imc <= 34.9) {
       return "Obesidade grau 1";
-    } else if (imc >= 35.0 && imc <= 39.9) {
+    } else if (imc > 34.9 && imc <= 39.9) {
       return "Obesidade grau 2";
     } else{
       return "Obesidade Extrema"
@@ -35,12 +36,16 @@ export default function App() {
 
   return (
     <div>
-      <h1>Calculadora IMC</h1>
-      <form onSubmit={classificação}>
+      <header className="cabecalho">
+        <h1>Calculadora IMC</h1>
+        <hr />
+      </header>
+      <form onSubmit={classificação} className="corpo">
         <input
           type="number"
           placeholder="Digite o seu peso"
           name="peso"
+          className="input"
           onChange={(e) => setPeso(parseFloat(e.target.value))}
         />
         <p></p>
@@ -48,12 +53,17 @@ export default function App() {
           type="number"
           placeholder="Digite a sua altura"
           name="altura"
+          className="input"
           onChange={(e) => setAltura(parseFloat(e.target.value) / 100)}
         />
         <p></p>
         <button type="submit">Calcular</button>
+        <Calculadora classificacao={res} />
       </form>
-      <Calculadora classificacao={res} />
+      <footer className="rodape">
+        <hr />
+        <h3>Aluno: Davi Soares Antunes</h3>
+      </footer>
     </div>
   );
 }
